e5d7c02302866ac687726468fdfa41c11f0659f3
ryoung141 <ryoung141@gmail.com>
2017-11-29 21:22:59 -0800

    init commit of models

---
+++ src/backend/models/accountHandle.java
@@ -0,0 +1,59 @@
+package StarsRUSModels
+
+import java.sql.*;
+
+class accountHandle
+{
+	final String connection = "jdbc:mysql://localhost:3306/cs174a";
+	ResultSet accountData;
+	int id;
+	String username;
+	String firstname;
+	String lastname;
+	String state_code;
+	String phonenumber;
+	String email;
+
+
+	public accountHandle(String username, String password)
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(this.connection);
+		Statement stmt = con.createStatement();
+		String query = "SELECT * FROM `customer_profile` 
+						WHERE `username`="+username+"
+						AND `password`="+password;
+		ResultSet rs = stmt.executeQuery(query);
+		this.accountData = rs;
+		this.id = rs.getInt("tax_id");
+		this.username = rs.getString("username");
+		this.firstname = rs.getString("firstname");
+		this.lastname = rs.getString("lastname");
+		this.state_code = rs.getString("state");
+		this.phonenumber = rs.getString("phonenumber");
+		this.email = rs.getString("email");
+	}
+
+	public accountHandle(int id)
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(this.connection);
+		Statement stmt = con.createStatement();
+		String query = "SELECT * FROM `customer_profile` 
+						WHERE `tax_id`="+id;
+		ResultSet rs = stmt.executeQuery(query);
+		this.accountData = rs;
+		this.id = rs.getInt("tax_id");
+		this.username = rs.getString("username");
+		this.firstname = rs.getString("firstname");
+		this.lastname = rs.getString("lastname");
+		this.state_code = rs.getString("state");
+		this.phonenumber = rs.getString("phonenumber");
+		this.email = rs.getString("email");
+	}
+
+	public static void main(String args[])
+	{
+		accountHandle ah = new accountHandle("admin", "admin");
+	}
+}
