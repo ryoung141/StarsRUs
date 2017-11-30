2092ca5c1d16a5b7108b5b73330e11e6b1511435
ryoung141 <ryoung141@gmail.com>
2017-11-29 21:30:20 -0800

    added try catch logic to accountHandle model

--- src/backend/models/accountHandle.java
+++ src/backend/models/accountHandle.java
@@ -1,4 +1,4 @@
-package StarsRUSModels
+package StarsRUSModels;
 
 import java.sql.*;
 
@@ -17,43 +17,51 @@ class accountHandle
 
 	public accountHandle(String username, String password)
 	{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection(this.connection);
-		Statement stmt = con.createStatement();
-		String query = "SELECT * FROM `customer_profile` 
-						WHERE `username`="+username+"
-						AND `password`="+password;
-		ResultSet rs = stmt.executeQuery(query);
-		this.accountData = rs;
-		this.id = rs.getInt("tax_id");
-		this.username = rs.getString("username");
-		this.firstname = rs.getString("firstname");
-		this.lastname = rs.getString("lastname");
-		this.state_code = rs.getString("state");
-		this.phonenumber = rs.getString("phonenumber");
-		this.email = rs.getString("email");
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(this.connection);
+			Statement stmt = con.createStatement();
+			String query = "SELECT * FROM customer_profile WHERE username="+username+" AND password="+password;
+			ResultSet rs = stmt.executeQuery(query);
+			this.accountData = rs;
+			this.id = rs.getInt("tax_id");
+			this.username = rs.getString("username");
+			this.firstname = rs.getString("firstname");
+			this.lastname = rs.getString("lastname");
+			this.state_code = rs.getString("state");
+			this.phonenumber = rs.getString("phonenumber");
+			this.email = rs.getString("email");
+			con.close();
+		}catch(Exception e){System.out.println(e);}
+		
 	}
 
 	public accountHandle(int id)
 	{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection(this.connection);
-		Statement stmt = con.createStatement();
-		String query = "SELECT * FROM `customer_profile` 
-						WHERE `tax_id`="+id;
-		ResultSet rs = stmt.executeQuery(query);
-		this.accountData = rs;
-		this.id = rs.getInt("tax_id");
-		this.username = rs.getString("username");
-		this.firstname = rs.getString("firstname");
-		this.lastname = rs.getString("lastname");
-		this.state_code = rs.getString("state");
-		this.phonenumber = rs.getString("phonenumber");
-		this.email = rs.getString("email");
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(this.connection);
+			Statement stmt = con.createStatement();
+			String query = "SELECT * FROM customer_profile WHERE tax_id="+id;
+			ResultSet rs = stmt.executeQuery(query);
+			this.accountData = rs;
+			this.id = rs.getInt("tax_id");
+			this.username = rs.getString("username");
+			this.firstname = rs.getString("firstname");
+			this.lastname = rs.getString("lastname");
+			this.state_code = rs.getString("state");
+			this.phonenumber = rs.getString("phonenumber");
+			this.email = rs.getString("email");
+			con.close();
+		}catch(Exception e){System.out.println(e);}
+		
 	}
 
 	public static void main(String args[])
 	{
 		accountHandle ah = new accountHandle("admin", "admin");
+		System.out.println(ah.firstname);
 	}
 }
