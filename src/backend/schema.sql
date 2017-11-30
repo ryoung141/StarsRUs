e545017c70e7faedf4fe7c36c7b0dbd381851bb9
ryoung141 <ryoung141@gmail.com>
2017-11-29 20:26:15 -0800

    schema error free

--- src/backend/schema.sql
+++ src/backend/schema.sql
@@ -1,3 +1,19 @@
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS customer_profile;
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS market_account;
+DROP TABLE IF EXISTS stock_account;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS buy;
+DROP TABLE IF EXISTS sell;
+DROP TABLE IF EXISTS withdrawal;
+DROP TABLE IF EXISTS deposit;
+DROP TABLE IF EXISTS accrue_interest;
+DROP TABLE IF EXISTS stock;
+DROP TABLE IF EXISTS actor;
+DROP TABLE IF EXISTS movie_contract;
+SET foreign_key_checks = 1;
+
 CREATE TABLE customer_profile(
 	tax_id int(100) UNIQUE, 
 	username varchar(255) PRIMARY KEY, 
@@ -8,13 +24,13 @@ CREATE TABLE customer_profile(
 			   "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN",
 			   "MO", "MS", "MY", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OK",
 			   "OH", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA",
-			   "WI", "WV", "WY")
+			   "WI", "WV", "WY"),
 	phonenumber varchar(100),
 	email varchar(255)
 	);
 
 CREATE TABLE account(
-	a_id int(100) AUTOINCREMENT,
+	a_id int(100) AUTO_INCREMENT,
 	owner varchar(255) NOT NULL,
 	PRIMARY KEY(a_id, owner),
 	FOREIGN KEY(owner)
@@ -69,10 +85,7 @@ CREATE TABLE buy(
   	amount float(13, 3) NOT NULL,
   	FOREIGN KEY (t_id)
     	REFERENCES transactions(t_id)
-    	ON UPDATE CASCADE ON DELETE CASCADE,
-  	FOREIGN KEY (s_id)
-    	REFERENCES stock(s_id)
-    	ON DELETE CASCADE
+    	ON UPDATE CASCADE ON DELETE CASCADE
 	);
 
 CREATE TABLE sell (
@@ -81,10 +94,7 @@ CREATE TABLE sell (
   	amount float(13, 3) NOT NULL,
   	FOREIGN KEY (t_id)
     	REFERENCES transactions(t_id)
-    	ON UPDATE CASCADE ON DELETE CASCADE,
-  	FOREIGN KEY (s_id)
-    	REFERENCES stock(s_id)
-    	ON DELETE CASCADE
+    	ON UPDATE CASCADE ON DELETE CASCADE
 	);
 
 CREATE TABLE accrue_interest (
@@ -94,10 +104,7 @@ CREATE TABLE accrue_interest (
   	avg_daily_balance float(13, 3) NOT NULL,
   	FOREIGN KEY (t_id)
     	REFERENCES transactions(t_id)
-    	ON UPDATE CASCADE ON DELETE CASCADE,
-  	FOREIGN KEY (s_id)
-    	REFERENCES stock(s_id)
-    	ON DELETE CASCADE
+    	ON UPDATE CASCADE ON DELETE CASCADE
   	);
   
 CREATE TABLE stock (
@@ -106,10 +113,7 @@ CREATE TABLE stock (
 	symbol char(3) NOT NULL,
 	closing_price int(100),
 	current_price int(100),
-	active enum('0', '1'),
-	FOREIGN KEY (a_id)
-	    REFERENCES actor(a_id)
-	    ON DELETE CASCADE
+	active enum('0', '1')
   );
   
 CREATE TABLE actor (
@@ -130,6 +134,18 @@ CREATE TABLE movie_contract (
 	year_released int(100),
 	total_value int(100) NOT NULL,
 	PRIMARY KEY(m_id, a_id),
-  	FOREIGN KEY a_id
-    	REFERENCES actor(a_id),
+  	FOREIGN KEY (a_id)
+    	REFERENCES actor(a_id)
   );
+
+ALTER TABLE stock
+	ADD FOREIGN KEY (a_id) REFERENCES actor(a_id) ON UPDATE CASCADE;
+
+ALTER TABLE buy
+	ADD FOREIGN KEY (s_id) REFERENCES stock(s_id) ON DELETE CASCADE;
+
+ALTER TABLE sell
+	ADD FOREIGN KEY (s_id) REFERENCES stock(s_id) ON DELETE CASCADE;
+
+ALTER TABLE accrue_interest
+	ADD FOREIGN KEY (s_id) REFERENCES stock(s_id) ON DELETE CASCADE;
