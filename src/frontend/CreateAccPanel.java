07252535939204634e94fbcb89fddb93a89a8e60
Jordan Ang <jordanangof18@gmail.com>
2017-12-03 01:18:16 -0800

    Input verification skeleton setup

--- src/frontend/CreateAccPanel.java
+++ src/frontend/CreateAccPanel.java
@@ -11,7 +11,7 @@ public class CreateAccPanel extends JPanel {
 	private JLabel lastNameLabel;
 	private JTextField lastNameField;
 	private JLabel StateLabel;
-	private JTextField StateField;
+	private JComboBox StateField;
 	private JLabel phoneNumLabel;
 	private JTextField phoneNumField1;
 	private JTextField phoneNumField2;
@@ -27,9 +27,63 @@ public class CreateAccPanel extends JPanel {
 	private JPasswordField passwordField;
 	private JLabel confirmPasswordLabel;
 	private JPasswordField confirmPasswordField;
+	private JLabel errorMessage;
 	
 
 	JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
+
+	String[] stateCodes = {
+								"AL",
+								"AK",
+								"AZ",
+								"AR",
+								"CA",
+								"CO",
+								"CT",
+								"DE",
+								"DC",
+								"FL",
+								"GA",
+								"HI",
+								"ID",
+								"IL",
+								"IN",
+								"IA",
+								"KS",
+								"KY",
+								"LA",
+								"ME",
+								"MT",
+								"NE",
+								"NV",
+								"NH",
+								"NJ",
+								"NM",
+								"NY",
+								"NC",
+								"ND",
+								"OH",
+								"OK",
+								"OR",
+								"MD",
+								"MA",
+								"MI",
+								"MN",
+								"MS",
+								"MO",
+								"PA",
+								"RI",
+								"SC",
+								"SD",
+								"TN",
+								"TX",
+								"UT",
+								"VT",
+								"VA",
+								"WA",
+								"WV",
+								"WI",
+								"WY"};
 
 	public CreateAccPanel() {
 		this.setLayout(null);
@@ -61,8 +115,8 @@ public class CreateAccPanel extends JPanel {
 		StateLabel.setBounds(200, base + 2*(35), 75, 25);
 		StateLabel.setHorizontalAlignment(JLabel.CENTER);
 
-		StateField = new JTextField();
-		StateField.setBounds(300, base + 2*(35), 25, 25);
+		StateField = new JComboBox(stateCodes);
+		StateField.setBounds(300, base + 2*(35), 50, 25);
 
 		phoneNumLabel = new JLabel("Phone #");
 		phoneNumLabel.setBounds(350, base + 2*(35), 75, 25);
@@ -115,7 +169,69 @@ public class CreateAccPanel extends JPanel {
 		createAccButton = new JButton("CREATE ACCOUNT");
 		createAccButton.setBounds(325, base + 8*(35), 150, 50);
 
+		errorMessage = new JLabel();
+		errorMessage.setHorizontalAlignment(JLabel.CENTER);
+		errorMessage.setBounds(200, base + 9*(35), 400, 50);
+
 		//Event listeners
+		createAccButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//Get inputs
+				String firstName = firstNameField.getText();
+				String lastName = lastNameField.getText();
+				String stateCode = String.valueOf(StateField.getSelectedItem());
+				String phoneNum = phoneNumField1.getText() + "-" 
+								+ phoneNumField2.getText() + "-" 
+								+ phoneNumField3.getText();
+				String email = emailField.getText();
+				String taxID = taxIdField.getText();
+				String username = usernameField.getText();
+				String password = passwordField.getText();
+				String confirmPass = confirmPasswordField.getText();
+
+				//Verify Inputs
+				errorMessage.setText("");
+
+				if(!verifyName(firstName)) {
+					errorMessage.setText("Error: First name must contain only characters");
+				}
+
+				if(!verifyName(lastName)) {
+					errorMessage.setText("Error: Last name must contain only characters");
+				}
+
+				if(!verifyPhoneNum(phoneNum)) {
+					errorMessage.setText("Error: Phone number must be formatted (000) 000-0000");
+				}
+
+				if(!verifyEmail(email)) {
+					errorMessage.setText("Error: Invalid email");
+				}
+
+				if(!verifyTaxId(taxID)) {
+					errorMessage.setText("Error: Invalid tax ID");
+				}
+
+				if(!verifyUsername(username)) {
+					errorMessage.setText("Error: Username can only contain letters and numbers");
+				}
+
+				if(!verifySamePass(password, confirmPass)) {
+					errorMessage.setText("Error: Passwords do not match");
+				}
+
+				//Debug
+				System.out.println(firstName);
+				System.out.println(lastName);
+				System.out.println(stateCode);
+				System.out.println(phoneNum);
+				System.out.println(email);
+				System.out.println(taxID);
+				System.out.println(username);
+				System.out.println(password);
+				System.out.println(confirmPass);
+			 }
+		});
 
 		//Add components
 		this.add(title);
@@ -140,8 +256,37 @@ public class CreateAccPanel extends JPanel {
 		this.add(confirmPasswordLabel);
 		this.add(confirmPasswordField);
 		this.add(createAccButton);
+		this.add(errorMessage);
 
 
+	}
+
+	public boolean verifyName(String name) {
+		return true;
+	} 
+
+	public boolean verifyPhoneNum(String phoneNum) {
+		return true;
+	}
+
+	public boolean verifyEmail(String email) {
+		return true;
+	}
+
+	public boolean verifyTaxId(String taxID) {
+		return true;
+	}
+
+	public boolean verifyUsername(String username) {
+		return true;
+	}
+
+	public boolean verifySamePass(String pass1, String pass2) {
+		if(pass1.equals(pass2)){
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
